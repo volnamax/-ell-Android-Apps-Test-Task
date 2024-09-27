e7265c8d5e8f4178b11ecc9107a4f2228dafcd06
max <mvolnyaga@gmail.com>
2024-09-27 12:53:43 +0300

    add di

--- app/src/main/java/com/example/cell_android_apps_test_task/di/ViewmodelModule.kt
+++ app/src/main/java/com/example/cell_android_apps_test_task/di/ViewmodelModule.kt
@@ -7,8 +7,7 @@ import org.koin.dsl.module
 
 
 val viewmodelModule = module {
-    CellsCreateViewModule(
-        cellRepository = get<ICellRepository>()
-    )
+    viewModel { CellsCreateViewModule(cellRepository = get<ICellRepository>()) }
+
 
 }
